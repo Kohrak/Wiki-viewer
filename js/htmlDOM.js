87ee52de26fb4cf97108ba0808bbf53ffe6f508d
Kohrak <gabrielgt95@gmail.com>
2018-02-11 01:01:19 +0000

    add links on results

--- js/htmlDOM.js
+++ js/htmlDOM.js
@@ -1,6 +1,7 @@
 function addResult(title, snippet, link){
   var element = document.createElement("div");
   element.setAttribute("class", "wiki-result");
+  element.setAttribute("onclick", "window.location.href=\'" + link + "\'");
   element.innerHTML = "<h3>" + title + "</h3>" + "<p>" + snippet + "</p>";
   document.body.appendChild(element);
 }
